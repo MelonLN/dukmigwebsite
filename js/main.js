8603bffd7372ccc1662d7386a690e3a154a38eba
MelonLN <huynhat2570@gmail.com>
2024-06-06 23:14:18 +0700

    update

--- js/main.js
+++ js/main.js
@@ -82,3 +82,60 @@ slider1.onmousemove = e => {
     }
 
 }
+
+//site load
+var originalContent = document.getElementById('content2');
+var content = document.getElementById('content');
+document.getElementById('contact-tab').addEventListener('click', function() {
+    loadPage('contact.html', originalContent, content);
+});
+document.getElementById('home-tab').addEventListener('click', function() {
+    goHome(originalContent, content);
+});
+document.getElementById('about-tab').addEventListener('click', function() {
+    loadPage('about.html', originalContent, content);
+});
+
+function loadPage(page, ogContent, content) {
+    fetch(page)
+      .then(response => response.text())
+      .then(data => {
+        content.style.display = 'block';
+        content.innerHTML = data;
+        ogContent.style.display = 'none';
+      });
+}
+
+function goHome(ogContent, content) {
+    ogContent.style.display = 'block';
+    content.innerHTML = '';
+    content.style.display = 'none';
+}
+
+//form
+function validateInput(input) {
+    if (input.value.trim() === '') {
+        input.classList.add('invalid');
+        document.getElementById('emailError').style.display = 'block';
+        document.getElementById('msgError').style.display = 'block';
+    } else {
+        input.classList.remove('invalid');
+        document.getElementById('emailError').style.display = 'none';
+        document.getElementById('msgError').style.display = 'none';
+    }
+}
+
+function validateForm(event) {
+    var emailInput = document.getElementById('infor-input-email');
+    var msgInput = document.getElementById('msg-input');
+    if (emailInput.value.trim() === '') {
+        event.preventDefault();
+        emailInput.classList.add('invalid');
+        document.getElementById('emailError').style.display = 'block';
+    }
+    if (msgInput.value.trim() === '') {
+        event.preventDefault();
+        msgInput.classList.add('invalid');
+        document.getElementById('msgError').style.display = 'block';
+    }
+}
